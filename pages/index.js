39f79cf14ceed5b89cf3030f1db8512f981805e4
Miguel-Cesar <ninjaariette@gmail.com>
2022-01-30 18:04:14 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+function Home() {
+    return (
+      <div>
+          <Head>
+              <meta charSet="utf-8" />
+              <meta name="robots" content="index, follow" />
+              <meta name="description" content="Site Teste Next.js" />
+              <meta name="author" content="Miguel - César" />
+              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+              <title>Welcome - Rome</title>
+          </Head>
+          você não leu nada.<br/>
+        Topo do site!
+        </div>
+    )
+  }
+  
+  export default Home;
